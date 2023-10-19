a6b70f17150e075ae0f96f52bd1e06dab723c90c
Zolsto <skywalker1244@yahoo.com>
2023-10-19 17:40:40 +0200

    Update esercizi 1

--- 01ex.py
+++ 01ex.py
@@ -1,6 +1,8 @@
 #esercizio 1
 for i in range(1,101):
-    if i%3==0:
+    if i%3==0 and i%5==0:
+        print("HelloWorld")
+    elif i%3==0:
         print("Hello")
     elif i%5==0:
         print("World")
