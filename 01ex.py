7893651e4b015079f156bd3943b39e39eeb1370a
Zolsto <skywalker1244@yahoo.com>
2023-10-25 10:48:43 +0200

    Update command line

--- 01ex.py
+++ 01ex.py
@@ -1,4 +1,5 @@
 #esercizio 1
+print("Esercizio 1\n")
 for i in range(1,101):
     if i%3==0 and i%5==0:
         print("HelloWorld")
@@ -10,12 +11,14 @@ for i in range(1,101):
         print(i)
 
 #esercizio 2
+print("\nEsercizio 2\n")
 x = input("X value: ")
 y = input("Y value: ")
 x, y = y, x
 print("After swap: X =", x, "Y =", y)
 
 #esercizio 3
+print("\nEsercizio 3\n")
 import math
 u = (0, 3)
 v = (-3, 5)
@@ -23,6 +26,7 @@ dist = math.sqrt(math.pow(u[0]-v[0], 2)+math.pow(u[1]-v[1], 2))
 print("%6.3f" %(dist))
 
 #esercizio 4
+print("\nEsercizio 4\n")
 s1 = "Write a program that prints the numbers from 1 to 100. \
 But for multiples of three print Hello instead of the number and for the multiples of five print World. \
 For numbers which are multiples of both three and five print HelloWorld."
@@ -46,6 +50,7 @@ for i in range(65, 91):
         c=0
 
 #esercizio 5
+print("\nEsercizio 5\n")
 l = [36, 45, 58, 3, 74, 96, 64, 45, 31, 10, 24, 19, 33, 86, 99, 18, 63, 70, 85,
  85, 63, 47, 56, 42, 70, 84, 88, 55, 20, 54, 8, 56, 51, 79, 81, 57, 37, 91,
  1, 84, 84, 36, 66, 9, 89, 50, 42, 91, 50, 95, 90, 98, 39, 16, 82, 31, 92, 41,
@@ -57,6 +62,7 @@ for i in range(0,len(l)):
 print(len(rep1), "unique elements, which are: ", rep1)
 
 #esercizio 6
+print("\nEsercizio 6\n")
 x = input("Insert X: ")
 y = input("Insert Y: ")
 try:
@@ -75,15 +81,18 @@ except:
     print("Sembra che la somma non sia possibile!")
 
 #esercizio 7
+print("\nEsercizio 7\n")
 cubi=[]
 for x in range(0, 10):
     cubi.append(x**3)
 print(cubi)
 
 #esercizio 8
+print("\nEsercizio 8\n")
 print([(x, y) for x in range(3) for y in range(4)])
 
 #esercizio 9
+print("\nEsercizio 9\n")
 import math
 pyth = []
 for a in range(1, 100):
@@ -94,6 +103,7 @@ for a in range(1, 100):
 print(pyth)
 
 #esercizio 10
+print("\nEsercizio 10\n")
 import math
 vettore = (10, 6, 34, 7, 89)
 somma_quad = sum(x**2 for x in vettore)
@@ -101,6 +111,7 @@ normalized = tuple(x/math.sqrt(somma_quad) for x in vettore)
 print(normalized)
 
 #esercizio 11
+print("\nEsercizio 11\n")
 fib=[1, 1]
 for i in range(2, 21):
     fib.append(fib[i-1]+fib[i-2])
