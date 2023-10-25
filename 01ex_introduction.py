ffc66aa72095614645431008106868496124a617
federico <federico.pilotto@studenti.unipd.it>
2023-10-25 15:32:46 +0200

    aggiornato

--- 01ex_introduction.py
+++ 01ex_introduction.py
@@ -4,7 +4,7 @@ import sys
 import math
 
 #esercizio 1
-print("Esercizio 1")
+print("Excercise 1")
 
 i = 1
 lista = []
@@ -27,14 +27,14 @@ print(tupla)
 print("")
 
 #esercizio 2
-print("Esercizio 2")
+print("Exercise 2")
 
 print("x = ", sys.argv[2], "y = ", sys.argv[1])
 print("")
 
 
 #esercizio 3
-print("Esercizio 3")
+print("Exercise 3")
 
 u = (3, 0)
 v = (0, 4)
@@ -46,12 +46,12 @@ dist = math.sqrt(diff1**2 + diff2**2)
 #oppure si può usare direttamente la funzione dist() della librearia math
 #dist2 = math.dist(u,v)
 
-print("Distanza euclidiana tra i punti : ", u, 'e ', v, '= ', dist)
+print("Euclidean distance between the two point : ", u, 'and ', v, '= ', dist)
 print("")
 
 
 #esercizio 4
-print("Esercizio 4")
+print("Exercise 4")
 
 s1 = "Write a program that prints the numbers from 1 to 100. \
 But for multiples of three print Hello instead of the number and for the multiples of five print World. \
@@ -67,7 +67,7 @@ for i in s1:
         freq[i] += 1
     else:
         freq[i] = 1
-print("Occorrenze lettere prima stringa: ", freq)
+print("Letters frequency of the first string: ", freq)
 print("")
 
 freq = {}
@@ -77,11 +77,11 @@ for i in s2:
     else:
         freq[i] = 1
         
-print("Occorrenze lettere seconda stringa: ", freq)
+print("Letters frequency of the secondo string: ", freq)
 print("")
 
 #esercizio 5
-print("Esercizio 5")
+print("Exercise 5")
 
 l = [36, 45, 58, 3, 74, 96, 64, 45, 31, 10, 24, 19, 33, 86, 99, 18, 63, 70, 85,
  85, 63, 47, 56, 42, 70, 84, 88, 55, 20, 54, 8, 56, 51, 79, 81, 57, 37, 91,
@@ -100,11 +100,11 @@ for i in l:
 for i in freq:
     if freq[i] == 1:
         listaUnici.append(i)
-print('Lista con i valori da una sola occorrenza\n', listaUnici)
+print('List of item with frequence = 1\n', listaUnici)
 print('')
 
 #esercizio 6
-print("Esercizio 6")
+print("Exercise 6")
 
 a = sys.argv[3]
 b = sys.argv[4]
@@ -112,74 +112,61 @@ try:
     a = float(a)
     b = float(b)
 
-    print('La somma  di ', a, ' e ', b, ' è: ', a + b)
+    print('The sum of ', a, ' and ', b, ' is: ', a + b)
 except:
-    print('Impossibile eseguire la somma di ', a, ' e ', b)
+    print('Impossible to do the sum of these items ', a, ' and ', b)
 print('')
 
 #esercizio 7
-print("Esercizio 7")
+print("Exercise 7")
 
 cubes = []
 for i in range(11):
     cubes.append(pow(i, 3))
-print('cubi dei numeri con for loop: ', cubes)
+print('Cubes done with for cycle: ', cubes)
 
 cubes = [x**3 for x in range(11)]
-print('cubi dei numeri con list comprehensions: ', cubes)
+print('Cubes done with list comprehensions: ', cubes)
 print('')
 
 #esercizio 8
-print("Esercizio 8")
+print("Exercise 8")
 
 list = [(i,j) for i in range(3) for j in range(4)]
 print(list)
 print('')
 
 #esercizio 9
-print("Esercizio 9")
-        
-'''
-list = []
-limit = 100
-for x in range(1, limit):
-    for y in range(1, x):
-        if (x-y) % 2 == 1 and math.gcd(x, y) == 1:
-            a = x**2 - y**2
-            b = 2*x*y
-            c = x**2 + y**2
-        if c < limit and (a, b, c) not in list:
-            list.append((a, b, c))
-'''
+print("Exercise 9")
 
 triples = [(a,b,c) for c in range(100) for a in range(c) for b in range(a) if a**2 + b**2 == c**2 and (a-b) % 2 == 1 and math.gcd(a,b) == 1]
 
-print('Terne pitagoriche con c < 100')
+print('Pythagorean triple with c < 100')
 print(triples)
 
 print('')
 
 #esercizio 10
-print("Esercizio 10")
+print("Exercise 10")
 
 vector = [1, 23, 4, 5, 12]
 for i in range(5, len(sys.argv)):
     vector.append(int(sys.argv[i]))
 
-print('vettore: ', vector)
+print('Vector: ', vector)
 somma = sum([x**2 for x in vector])
 normalized = [x/somma for x in vector]
-print('Vettore normalizzato:\n', normalized)
+print('Normalized Vector:', normalized)
 print('')
 
 #esercizio 11
-print("Esercizio 11")
+print("Exercise 11")
 
 fibo = [1, 1]
 for i in range(2, 20):
      fibo.append(fibo[i-1] + fibo[i-2])
 
-print('Sequenza Fibonacci dei primi 20 numeri')
+print('Fibonacci sequence of the first 20 numbers')
 print(fibo)
 
 
--- 02ex_fundamentals.py
+++ 02ex_fundamentals.py
@@ -1,7 +1,7 @@
 import timeit
 
 #esercizio 1
-print('Esericizio 1')
+print('Exercise 1')
 
 def f(alist, n):
     temp = alist.copy()
@@ -10,44 +10,44 @@ def f(alist, n):
     return temp
 
 alist = [1, 2, 3]
-print('Lista modificata: ', f(alist, 5))
-print('Lista originale: ', alist)
+print('Modify list: ', f(alist, 5))
+print('Original list: ', alist)
 print('')
 
 #esercizio 2
-print('Esericizo 2')
+print('Exercise 2')
 ans = list(map(lambda x: x * x, filter(lambda x: x % 2 == 1, range(10))))
 lista = [x**2 for x in range(10) if x % 2 == 1]
-print('Lista data\n', ans)
-print('Lista con list comprehension\n', lista)
+print('Given list\n', ans)
+print('List with list comprehension\n', lista)
 print('')
 
 #esercizio 3
-print('Esericizio 3')
+print('Exercise 3')
 n = 6
 words = ['pippo', 'pluto', 'paperino', 'caprino', 'cagnolino', 'lettino', 'fiumicino', 'tapettino', 'scovolino', 'camino']
 
-print('Risultato atteso')
+print('Attended result: ',words[0], ' ', words[1])
 
-print('Dopo aver chiamato filter: \n', list(filter(lambda words: len(words) < n, words)))
+print('After calling the HOF filter: ', list(filter(lambda words: len(words) < n, words)))
 print('')
 
 #esercizio 4
-print('Esericizio 4')
+print('Exercise 4')
 
 lang = {"Python" : 3, "Java" : '', "Cplusplus" : 'test', "Php" : 0.7}
 print(list(map(lambda lang: len(lang), lang)))
 print('')
 
 #esercizio 5
-print('Esericizio 5')
+print('Exercise 5')
 
 language_scores = [('Python', 97), ('Cplusplus', 81), ('Php', 45), ('Java', 32)]
-print('Lista ordinata\n', sorted(language_scores, key=lambda x: x[0]))
+print('Sorted list\n', sorted(language_scores, key=lambda x: x[0]))
 print('')
 
 #esercizio 6
-print('Esericizio 6')
+print('Exercise 6')
 
 def square(x):
     return x**2
@@ -60,7 +60,7 @@ print('2 power to 6: ', power6(2))
 print('')
 
 #esercizio 7
-print('Esericizio 7')
+print('Exercise 7')
 
 def hello(func):
     def wrapper(x):
@@ -76,7 +76,7 @@ square(5)
 print('')
 
 #esercizio 8
-print('Esericizio 8')
+print('Exercise 8')
 def rec_fib(n):
     if n <= 1:
         return n
@@ -89,7 +89,7 @@ print(fibo)
 print('')
 
 #esercizio 9
-print('Esericizio 9')
+print('Exercise 9')
 
 def iter_fib(n):
     fibo = [1, 1]
@@ -97,20 +97,20 @@ def iter_fib(n):
         fibo.append(fibo[i-1] + fibo[i-2])
     return fibo
 
-#imposto numero di esecuzioni a 10000 perché con il valore di default
-#rec_fibonacci(n) impegnava il computer per troppo tempo
+#I set the number of execution to 10000 because the default number (1 milion) requires
+#too much time to test rec_fib(n)
 loop = 10000
 
 result = timeit.timeit(lambda: iter_fib(20), number=loop)
 result2 = timeit.timeit(lambda: rec_fib(20), number=loop)
-print('Tempo per ', loop, ' esecuzioni di fibonacci iterativo: ', result)
-print('Tempo per ', loop, ' esecuzioni di fibonacci ricorsivo: ', result2)
-print("L'algoritmo iterativo è decsamente il più efficiente! ->", int(result2/result), ' volte più veloce')
+print('Time for ', loop, ' eecution of iterative fibonacci: ', result)
+print('Time for ', loop, ' execution of recursive fibonacci: ', result2)
+print("The iterative algorithm is much more efficient then the recursive one! ->", int(result2/result), ' times faster')
 print('')
 
 
 #esercizio 10
-print('Esericizio 10')
+print('Exercise 10')
 
 class Polygon:
     lati = ()
@@ -118,7 +118,7 @@ class Polygon:
         if len(iLati) >= 3:
             self.lati = iLati
         else:
-            print('Il poligono deve avere almeno 3 lati')
+            print('The polygon must has at least 3 items')
 
     def getDimension(self):
         return len(self.lati)
@@ -144,19 +144,19 @@ class Polygon:
         else:
             return sorted(self.lati, reverse = True)
 
-print('Test classe Polygon')
+print('Test class Polygon')
 
 poly = Polygon((9, 3, 4, 5, 10))
 
-print('Perimetro: ', poly.perimeter())
-print('Lati ordinati: ', poly.getOrderedLength(False))
+print('Perimeter: ', poly.perimeter())
+print('Sorted items: ', poly.getOrderedLength(False))
 poly.setLength(2, 7)
-print('Modifico un lato: ', poly.lati)
-print('Nuovo perimetro: ', poly.perimeter())
+print('We modified one item: ', poly.lati)
+print('New perimeter: ', poly.perimeter())
 print('')
 
 #esercizio 11
-print('Esericizio 11')
+print('Exercise 11')
 
 class Rectangle(Polygon):
 
@@ -164,7 +164,7 @@ class Rectangle(Polygon):
         if len(iLati) == 4:
             self.lati = iLati
         else:
-            print('Bisogna inserire i lati nel seguente formato: a b a b')
+            print('You must follow the following format: a b a b')
 
 
     def area(self):
@@ -172,6 +172,6 @@ class Rectangle(Polygon):
 
 
 rect = Rectangle((5,10, 5, 10))
-print('Test classe Rectangle: base: ', rect.lati[0], ' altezza: ', rect.lati[1])
+print('Test classe Rectangle: base: ', rect.lati[0], ' high: ', rect.lati[1])
 print('Area: ', rect.area())
-print('Perimetro: ', rect.perimeter())
+print('Perimeter: ', rect.perimeter())
